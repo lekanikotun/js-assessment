c792309c4f8d1ee5ca07b145d0a370f98d4452f6
Lekan Ikotun <lekanikotun@gmail.com>
2015-09-30 16:31:12 -0400

    submission

--- app/count.js
+++ app/count.js
@@ -3,5 +3,12 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.countAnswers =  {
   count : function (start, end) {
 
-  }
+	while (start <= end) {
+		(function(n)
+	  	setTimeout(function() {
+  			console.log(n);
+	  	}, 100);
+
+	  }(start++));
+	}
 };
